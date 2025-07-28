3664222a9dd3cf63fd2f5e985799955597498940
Srushti325 <shrushtikulkarni828@gmail.com>
2025-07-29 00:25:58 +0530

    new changes

--- MinMaxInArray.java
+++ MinMaxInArray.java
@@ -25,6 +25,22 @@ public class MinMaxInArray {
 
         return min;
     }
+
+    // Both together
+    public static void minMax(int arr []) {
+        int max = arr[0];
+        int min = arr[0];
+        for(int i=1; i<arr.length; i++) {
+            if(arr[i]>max) {
+                max = arr[i];
+            }
+            if(arr[i]<min) {
+                min = arr[i];
+            }
+        }
+        System.out.println("Max element is: "+max);
+        System.out.println("Min element is: "+min);
+    }
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
 
@@ -38,9 +54,11 @@ public class MinMaxInArray {
             arr[i] = sc.nextInt();
         }
 
-        System.out.println();
-        System.out.println("Maximum element in the array is: " + max(arr));
-        System.out.println("Minimum element in the array is: " + min(arr));
+        // System.out.println();
+        // System.out.println("Maximum element in the array is: " + max(arr));
+        // System.out.println("Minimum element in the array is: " + min(arr));
+
+        minMax(arr);
 
 
     }
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,3 @@
+        // System.out.println();
+        // System.out.println("Maximum element in the array is: " + max(arr));
+        // System.out.println("Minimum element in the array is: " + min(arr));
